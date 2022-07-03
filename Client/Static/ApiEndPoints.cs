be03f5a92ce26a60d1129552773e2261238875cd
khalid <5alidghoddane@gmail.com>
2022-07-03 01:05:01 +0100

    fix APIENDPOINT and categorycard

--- Client/Static/ApiEndPoints.cs
+++ Client/Static/ApiEndPoints.cs
@@ -2,13 +2,8 @@
 
 internal static class ApiEndPoints
 {
-    #if DEBUG
-    //when under developement Producing
-    internal const string  ServerBaseUrl = "https://localhost:5003";
-    #else
-    //when production state
+
     internal const string  ServerBaseUrl = "https://valravnserver.azurewebsites.net";
-    #endif
 
     internal static readonly string SCategories = $"{ServerBaseUrl}/api/categories";
 }
