3564f9b9eec8d1959e456cc455c1bffae556f918
khalid <5alidghoddane@gmail.com>
2022-07-09 10:54:36 +0100

    finish Category Controller and fix object referencing recycling .

--- Server/Controllers/CategoriesController.cs
+++ Server/Controllers/CategoriesController.cs
@@ -9,10 +9,12 @@ namespace Server.Controllers;
 public class CategoriesController : Controller
 {
     private readonly AppDataContext _appDataContext;
+    private readonly IWebHostEnvironment _webHostEnvironment;
 
-    public CategoriesController(AppDataContext appDataContext)
+    public CategoriesController(AppDataContext appDataContext , IWebHostEnvironment webHostEnvironment)
     {
         _appDataContext = appDataContext;
+        _webHostEnvironment = webHostEnvironment;
     }
 
     #region Action Methods
@@ -45,6 +47,128 @@ public class CategoriesController : Controller
     {
         Category category = await GetCategoriesByCategoryId(id,true);
         return Ok(category);
+    }
+    
+    // create category method
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] Category? categoryToCreate)
+    {
+        try
+        {
+            if (categoryToCreate == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _appDataContext.Categories.AddAsync(categoryToCreate);
+            bool changesPersistToDatabase = await PersistChangesToDatabase();
+            if (!changesPersistToDatabase)
+            {
+                return StatusCode(500,
+                    "Something went wrong from our side please contact administrator.");
+            }
+            else
+            {
+                return Created("Create", categoryToCreate);
+            }
+            
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500,
+                $"Something went wrong from our side please contact administrator. Error Message: {e.Message}.");
+        }
+    } 
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id,[FromBody] Category? categoryToUpdate)
+    {
+        try
+        {
+            if (id<1 || categoryToUpdate == null || id != categoryToUpdate.CategoryId)
+            {
+                return BadRequest(ModelState);
+            }
+
+            bool exists = await _appDataContext.Categories.AnyAsync(c => c.CategoryId == id);
+            if (!exists)
+            {
+                return BadRequest(ModelState);
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _appDataContext.Categories.Update(categoryToUpdate);
+            bool changesPersistToDatabase = await PersistChangesToDatabase();
+            if (!changesPersistToDatabase)
+            {
+                return StatusCode(500,
+                    "Something went wrong from our side please contact administrator.");
+            }
+            else
+            {
+                return NoContent();
+            }
+            
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500,
+                $"Something went wrong from our side please contact administrator. Error Message: {e.Message}.");
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        try
+        {
+            if (id<1)
+            {
+                return BadRequest(ModelState);
+            }
+
+            bool exists = await _appDataContext.Categories.AnyAsync(c => c.CategoryId == id);
+            if (!exists)
+            {
+                return BadRequest(ModelState);
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Category categoryToDelete = await GetCategoriesByCategoryId(id, false);
+            if (categoryToDelete.ThumbnailImagePath != "uploads/placeholder.jpg")
+            {
+                string fileName = categoryToDelete.ThumbnailImagePath.Split('/').Last();
+                System.IO.File.Delete($"{_webHostEnvironment.ContentRootPath}\\wwwroot\\uploads\\{fileName}");
+            }
+
+            _appDataContext.Categories.Remove(categoryToDelete);
+            bool changesPersistToDatabase = await PersistChangesToDatabase();
+            if (!changesPersistToDatabase)
+            {
+                return StatusCode(500,
+                    "Something went wrong from our side please contact administrator.");
+            }
+            else
+            {
+                return NoContent();
+            }
+            
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500,
+                $"Something went wrong from our side please contact administrator. Error Message: {e.Message}.");
+        }
     }
     #endregion
 
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,7 +18,7 @@ builder.Services.AddCors(options =>
         .AllowAnyHeader());
 });
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(options=> options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
